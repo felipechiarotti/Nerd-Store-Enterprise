aa9cff280eb714011942d1dc036d5915feb5f1f2
Felipe Chiarotti Giron <felipechi97@gmail.com>
2021-11-16 12:50:35 -0300

    Tratamento de Erros do Servidor Parte 1

---
+++ src/web/NSE.WebApp.MVC/Extensions/ExceptionMiddleware.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSE.WebApp.MVC.Extensions
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpcontext)
+        {
+            try
+            {
+                await _next(httpcontext);
+            }catch(CustomHttpRequestException ex)
+            {
+                HandleRequestExceptionAsync(httpcontext, ex);
+            }
+        }
+
+        private static void HandleRequestExceptionAsync(HttpContext context, CustomHttpRequestException httpRequestException)
+        {
+            if(httpRequestException.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+            {
+                context.Response.Redirect("/login");
+                return;
+            }
+            context.Response.StatusCode = (int)httpRequestException.StatusCode;
+        }
+    }
+}
