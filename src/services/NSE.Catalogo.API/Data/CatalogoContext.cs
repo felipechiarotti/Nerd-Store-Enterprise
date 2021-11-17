27f3bd5ccaa630855bac1e84a93493f952fa98e6
Felipe Chiarotti Giron <felipechi97@gmail.com>
2021-11-17 17:09:50 -0300

    Configuração de repositórios

--- src/services/NSE.Catalogo.API/Data/CatalogoContext.cs
+++ src/services/NSE.Catalogo.API/Data/CatalogoContext.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using NSE.Catalogo.API.Models;
+using NSE.Core.Data;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace NSE.Catalogo.API.Data
 {
-    public class CatalogoContext : DbContext
+    public class CatalogoContext : DbContext, IUnitOfWork
     {
         public CatalogoContext(DbContextOptions<CatalogoContext> options) : base(options) { }
 
@@ -11,7 +14,13 @@ namespace NSE.Catalogo.API.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(
+                e => e.GetProperties().Where(p => p.ClrType == typeof(string))))
+                property.SetColumnType("varchar(100)");
+
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(CatalogoContext).Assembly);
         }
+
+        public async Task<bool> Commit() => await base.SaveChangesAsync() > 0;
     }
 }
---
+++ src/services/NSE.Catalogo.API/Models/IProdutoRepository.cs
@@ -0,0 +1,8 @@
+﻿using NSE.Core.Data;
+
+namespace NSE.Catalogo.API.Models
+{
+    public interface IProdutoRepository : IRepository<Produto>
+    {
+    }
+}
