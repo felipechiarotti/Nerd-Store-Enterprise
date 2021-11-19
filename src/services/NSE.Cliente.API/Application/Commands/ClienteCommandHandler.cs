f2472549ab4ad86532f60576df0f94aca37e0d5a
Felipe Chiarotti Giron <felipechi97@gmail.com>
2021-11-19 17:21:37 -0300

    Configuração Mediator e CommandHandler

--- src/services/NSE.Cliente.API/Application/Commands/ClienteCommandHandler.cs
+++ src/services/NSE.Cliente.API/Application/Commands/ClienteCommandHandler.cs
@@ -1,11 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿
+
+using FluentValidation.Results;
+using MediatR;
+using NSE.Clientes.API.Models;
+using NSE.Core.Messages;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace NSE.Clientes.API.Application.Commands
 {
-    public class ClienteCommandHandler
+    public class ClienteCommandHandler : CommandHandler, IRequestHandler<RegistrarClienteCommand, ValidationResult>
     {
+        public async Task<ValidationResult> Handle(RegistrarClienteCommand message, CancellationToken cancellationToken)
+        {
+            if (!message.EhValido())
+                return message.ValidationResult;
+
+            var cliente = new Cliente(message.Id, message.Nome, message.Email, message.Cpf);
+
+
+            return message.ValidationResult;
+        }
     }
 }
