8d507c86e8bb8e8005a10d5d01cf8bde0067bc39
Felipe Chiarotti Giron <felipechi97@gmail.com>
2021-12-05 15:45:48 -0300

    Desenvolvida a Base da Arquitetura de Pedidos

---
+++ src/services/NSE.Pedidos.Domain/Vouchers/Enums/TipoDescontoVoucherEnum.cs
@@ -0,0 +1,8 @@
+﻿namespace NSE.Pedidos.Domain.Vouchers.Enums
+{
+    public enum TipoDescontoVoucherEnum
+    {
+        Porcentagem = 0,
+        Valor = 1
+    }
+}
---
+++ src/services/NSE.Pedidos.Infra/Data/Repository/VoucherRepository.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NSE.Core.Data;
+using NSE.Pedidos.Domain.Vouchers.Entities;
+using NSE.Pedidos.Domain.Vouchers.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSE.Pedidos.Infra.Data.Repository
+{
+    public class VoucherRepository : IVoucherRepository
+    {
+        private readonly PedidosContext _context;
+
+        public VoucherRepository(PedidosContext context)
+        {
+            _context = context;
+        }
+
+        public IUnitOfWork UnitOfWork => _context;
+
+        public async void Adicionar(Voucher item)
+        {
+            await _context.Vouchers.AddAsync(item);
+        }
+
+        public void Atualizar(Voucher item)
+        {
+            _context.Vouchers.Update(item);
+        }
+
+        public async Task<Voucher> ObterPorIdAsync(Guid id)
+        {
+            return await _context.Vouchers.FirstOrDefaultAsync(v => v.Id == id);
+        }
+
+        public async Task<IEnumerable<Voucher>> ObterTodosAsync()
+        {
+            return await _context.Vouchers.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<Voucher> ObterVoucherPorCodigo(string codigo)
+        {
+            return await _context.Vouchers.FirstOrDefaultAsync(v => v.Codigo == codigo);
+        }
+
+        public void Dispose()
+            => _context.Dispose();
+    }
+}
