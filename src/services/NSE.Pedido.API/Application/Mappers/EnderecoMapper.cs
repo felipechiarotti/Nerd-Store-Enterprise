df1cdbfc74217a7302ebc7965e8d79052973bb88
Felipe Chiarotti Giron <felipechi97@gmail.com>
2021-12-08 11:27:39 -0300

    Desenvolver Camada Aplicação Pedido

---
+++ src/services/NSE.Pedido.API/Application/Mappers/EnderecoMapper.cs
@@ -0,0 +1,33 @@
+﻿using NSE.Pedidos.API.Application.DTO;
+using NSE.Pedidos.Domain.Pedidos.Entities;
+
+
+namespace NSE.Pedidos.API.Application.Mappers
+{
+    public class EnderecoMapper
+    {
+        public static EnderecoDTO ParaEnderecoDTO(Endereco endereco) =>
+            new EnderecoDTO()
+            {
+                Logradouro = endereco.Logradouro,
+                Numero = endereco.Numero,
+                Complemento = endereco.Complemento,
+                Bairro = endereco.Bairro,
+                Cep = endereco.Cep,
+                Cidade = endereco.Cidade,
+                Estado = endereco.Estado
+            };
+
+        public static Endereco ParaEndereco(EnderecoDTO enderecoDTO) =>
+             new Endereco
+             {
+                 Logradouro = enderecoDTO.Logradouro,
+                 Numero = enderecoDTO.Numero,
+                 Complemento = enderecoDTO.Complemento,
+                 Bairro = enderecoDTO.Bairro,
+                 Cep = enderecoDTO.Cep,
+                 Cidade = enderecoDTO.Cidade,
+                 Estado = enderecoDTO.Estado
+             };
+    }
+}
