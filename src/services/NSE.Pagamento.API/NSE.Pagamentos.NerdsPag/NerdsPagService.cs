94bca9932221d4fcab61bcbcc45c6fe5ad260267
Felipe Chiarotti Giron <felipechi97@gmail.com>
2021-12-20 11:47:40 -0300

    Implementar Base API Pagamentos

---
+++ src/services/NSE.Pagamento.API/NSE.Pagamentos.NerdsPag/NerdsPagService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NSE.Pagamentos.NerdsPag
+{
+    public class NerdsPagService
+    {
+        public readonly string ApiKey;
+        public readonly string EncryptionKey;
+
+        public NerdsPagService(string apiKey, string encryptionKey)
+        {
+            ApiKey = apiKey;
+            EncryptionKey = encryptionKey;
+        }
+    }
+}
